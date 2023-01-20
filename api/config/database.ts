515cf8801912ba1d10c6e7dbb7a66804f46803c5
Ankit Bhawnani <bbbneo333@gmail.com>
2023-01-20 14:56:24 +0530

    added support for DATABASE_SINGLE_CONN

--- api/config/database.ts
+++ api/config/database.ts
@@ -13,6 +13,12 @@ module.exports = ({ env }) => ({
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
     },
+    // DATABASE_SINGLE_CONN enforces single connection and disables connection pooling
+    // see - https://docs.strapi.io/developer-docs/latest/setup-deployment-guides/configurations/required/databases.html#configuration-structure
+    pool: env('DATABASE_SINGLE_CONN') !== 'true' ? null : {
+      min: 1,
+      max: 1,
+    },
     debug: false,
   },
 });
